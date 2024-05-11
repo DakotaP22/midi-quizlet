5404e090b1f0bb20eee6066e46ec0ab3f5a902b2
Dakota Pease <peased22@gmail.com>
2024-05-10 23:48:32 -0400

    feat(study): add basic layout for page

--- src/app/features/study/pages/study-page/study-page.component.ts
+++ src/app/features/study/pages/study-page/study-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PianoOverlayComponent } from '../../../../shared/components/piano-overlay/piano-overlay.component';
 
 @Component({
   selector: 'app-study-page',
   standalone: true,
-  imports: [],
+  imports: [PianoOverlayComponent],
   templateUrl: './study-page.component.html',
   styleUrl: './study-page.component.scss'
 })
