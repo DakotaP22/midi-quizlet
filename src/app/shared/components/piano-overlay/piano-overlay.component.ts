69fc7abbb9d84e63fe2f2b5ca50ee307f38aa3bb
Dakota Pease <peased22@gmail.com>
2024-05-10 23:27:08 -0400

    feat(shared): add placeholder piano overlay component

---
+++ src/app/shared/components/piano-overlay/piano-overlay.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'piano-overlay',
+  standalone: true,
+  imports: [],
+  templateUrl: './piano-overlay.component.html',
+  styleUrl: './piano-overlay.component.scss'
+})
+export class PianoOverlayComponent {
+
+}
