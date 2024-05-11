07fac0afd5f75ffd801a301e28cbfee27d99e483
Dakota Pease <peased22@gmail.com>
2024-05-11 01:48:26 -0400

    feat(study): add StudyCardComponent w/ hardcoded data

--- src/app/features/study/pages/study-page/study-page.component.ts
+++ src/app/features/study/pages/study-page/study-page.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { PianoOverlayComponent } from '../../../../shared/components/piano-overlay/piano-overlay.component';
+import { StudyCardComponent } from '../../components/study-card/study-card.component';
+import { StudyCardData } from '../../components/study-card/study-card.types';
 
 @Component({
   selector: 'app-study-page',
   standalone: true,
-  imports: [PianoOverlayComponent],
+  imports: [PianoOverlayComponent, StudyCardComponent],
   templateUrl: './study-page.component.html',
   styleUrl: './study-page.component.scss'
 })
 export class StudyPageComponent {
-
+  cardData: StudyCardData = {
+    isChord: true,
+    chordOrNoteName: 'C Major',
+    notes: ['C', 'E', 'G'],
+  }
 }
