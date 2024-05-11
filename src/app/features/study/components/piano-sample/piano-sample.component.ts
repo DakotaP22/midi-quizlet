37b70f4d4af8ab02ea280a3ab22e6f0ea01d0f40
Dakota Pease <peased22@gmail.com>
2024-05-11 18:20:50 -0400

    refactor(study): break out all keys from those expected to be pressed into seperate signals

--- src/app/features/study/components/piano-sample/piano-sample.component.ts
+++ src/app/features/study/components/piano-sample/piano-sample.component.ts
@@ -2,45 +2,47 @@ import { Component, computed, signal } from '@angular/core';
 import { Chord } from '../../../../midi/instruments/types/Chord';
 import { Note } from '../../../../midi/instruments/types/Note';
 import * as Piano from '../../../../midi/instruments/Piano';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'piano-sample',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './piano-sample.component.html',
-  styleUrl: './piano-sample.component.scss'
+  styleUrl: './piano-sample.component.scss',
 })
 export class PianoSampleComponent {
-  // expected = signal<Note | Chord>({root: {note: 'C', octave: 0}, type: 'major'} as Chord);
-  expected = signal<Note | Chord>({note: 'C', octave: 3} as Note);
+  expected = signal<Note | Chord>({root: {note: 'C', octave: 0}, type: 'major'} as Chord);
+  // expected = signal<Note | Chord>({ note: 'C', octave: 3 } as Note);
 
-  keys = computed<number[]>(() => {
+  keysToPress = computed<number[]>(() => {
     const noteOrChord = this.expected();
     const note = noteOrChord as Note;
     const chord = noteOrChord as Chord;
 
-    const padKeys = (keys: number[]) => {
-      const min = Math.min(...keys);
-      const max = Math.max(...keys);
-
-      const padded = Array.from({length: max - min + 3}, (_, i) => i + min - 1);
-      if(Piano.keyIsAccidental(padded[0])) {
-        padded.unshift(padded[0] - 1);
-      }
-
-      if(Piano.keyIsAccidental(padded[padded.length - 1])) {
-        padded.push(padded[padded.length - 1] + 1);
-      }
-      return padded;
-    }
-
-    if(chord?.root) {
+    if (chord?.root) {
       // chord
-      chord.root.octave = 3;
-      return padKeys(Piano.getKeys(chord))
+      chord.root.octave = 1;
+      return Piano.getKeys(chord);
     } else {
       // note
-      return padKeys([Piano.getKey(note)]);
+      return [Piano.getKey(note)];
     }
+  });
+
+  allKeys = computed<number[]>(() => {
+    const keys = this.keysToPress();
+    const min = Math.min(...keys);
+    const max = Math.max(...keys);
+
+    const padded = Array.from({ length: max - min + 3 }, (_, i) => i + min - 1);
+    if (Piano.keyIsAccidental(padded[0])) {
+      padded.unshift(padded[0] - 1);
+    }
+
+    if (Piano.keyIsAccidental(padded[padded.length - 1])) {
+      padded.push(padded[padded.length - 1] + 1);
+    }
+    return padded;
   });
 }
