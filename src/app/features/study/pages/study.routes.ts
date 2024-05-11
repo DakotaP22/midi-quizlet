9df85ab914e761794b23905413b15638941c2ee9
Dakota Pease <peased22@gmail.com>
2024-05-10 22:27:34 -0400

    fix(study): /study route loads study page with loadComponent

--- src/app/features/study/pages/study.routes.ts
+++ src/app/features/study/pages/study.routes.ts
@@ -3,6 +3,6 @@ import { Routes } from "@angular/router";
 export const studyRoutes: Routes = [
   {
     path: '',
-    loadChildren: () => import ('../pages/study-page/study-page.component').then(m => m.StudyPageComponent)
+    loadComponent: () => import ('../pages/study-page/study-page.component').then(m => m.StudyPageComponent)
   }
 ];
